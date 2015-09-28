fec0a95d4fca9e29bf2c00125d91094554539ea8
Alan Crawford <a.crawford.11@ucl.ac.uk>
2015-09-28 18:07:20 +0100

    Fixed up Smolyak Interpolant bugs

--- src/smolpoly.jl
+++ src/smolpoly.jl
@@ -18,11 +18,11 @@ function make_d2fdx2(sb::SmolyakBasis, coef::Vector{Float64},n::Int64=sb.D)
 end
 
 type SmolyakPoly
-	coef 	:: Vector{Float64}
-	f  		:: ScalarOrVec{Float64} 	# Value of Interpolant at each of the sb.NumPts
-	dfdx	:: Array{Vector{Float64},1} # Jacobian  where each element a vector of sb.NumPts x 1 wrt [i]
-	d2fdx2	:: Array{Vector{Float64},2} # Hessian where each element a vector of sb.NumPts x 1 for cross derivative [i,j] where i,j ∈{1,...,sb.D} 
-	
+	coef 	 :: Vector{Float64}
+	f  		 :: ScalarOrVec{Float64} 		# Value of Interpolant at each of the sb.NumPts
+	dfdx	 :: Array{Vector{Float64},1} 	# Jacobian  where each element a vector of sb.NumPts x 1 wrt [i]
+	d2fdx2	 :: Array{Vector{Float64},2} 	# Hessian where each element a vector of sb.NumPts x 1 for cross derivative [i,j] where i,j ∈{1,...,sb.D} 
+	NumDeriv :: Int64 						# Number of Derivative
 	function SmolyakPoly(sb::SmolyakBasis, coef::Vector, NumDeriv::Int64=sb.NumDeriv)		
 		if is(NumDeriv,2)
 			f = At_mul_B(sb.BF,coef) 
@@ -37,9 +37,9 @@ type SmolyakPoly
 			dfdx = [Array(Float64,sb.NumPts) for i in 1:sb.D]
 			d2fdx2 = [Array(Float64,sb.NumPts) for i in 1:sb.D, j in sb.D]
 		else
-			println("Require: 0 <= Number of derivatives <= 2")
+			println("Require: 0,1, or, 2 derivatives")
 		end
-		new(coef, f, dfdx, d2fdx2)
+		new(coef, f, dfdx, d2fdx2, NumDeriv)
 	end
 
 end
@@ -52,14 +52,14 @@ end
 # In place 1st Derivative Update
 function dfdx!(sp::SmolyakPoly,sb::SmolyakBasis,n::Int64=sb.D,coef::Vector{Float64}=sp.coef)
 	for d in 1:n
-		At_mul_B!(sb.dfdx[d],sb.dBFdx[d],coef)
+		At_mul_B!(sp.dfdx[d],sb.dBFdx[d],coef)
 	end
 end
 
 # In place 2nd Derivative Update
 function d2fdx2!(sp::SmolyakPoly,sb::SmolyakBasis,n::Int64=sb.D,coef::Vector{Float64}=sp.coef)
 	for j in 1:n, i in 1:n
-		At_mul_B!(sb.dfdx[i,j],sb.d2BFdx2[i,j],coef)
+		At_mul_B!(sp.d2fdx2[i,j],sb.d2BFdx2[i,j],coef)
 	end
 end
 
@@ -82,7 +82,13 @@ function SmolyakPoly!(sp::SmolyakPoly,sb::SmolyakBasis,n::Int64=sb.D,coef::Vecto
 	end
 end
 
-
+function show(io::IO, sp::SmolyakPoly)
+	msg = "\n\tCreated Smolyak Interpolant:\n"
+	if ==(sp.NumDeriv,0) msg *= "\t- No Derivative supplied. Do not call dfdx or d2fdx2.\n" end
+	if ==(sp.NumDeriv,1) msg *= "\t- with dfdx. Do not call d2fdx2.\n" end
+	if ==(sp.NumDeriv,2) msg *= "\t- with dfdx & d2dx2.\n" end
+	print(io, msg)
+end
 
 
 
