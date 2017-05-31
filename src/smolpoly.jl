a7355fd27ef7cb4f76213f3445992157478a1c63
Alan Crawford <a.crawford.11@ucl.ac.uk>
2017-05-31 23:46:31 +0100

    New Type Stable Version of Smolpoly

--- src/smolpoly.jl
+++ src/smolpoly.jl
@@ -93,7 +93,7 @@ end
 
 # In place fn value update
 function makeValue!(sp::SmolyakPoly,sb::SmolyakBasis;Coef::Vector{Float64}=sp.Coef)
-	sp.Value[n] = 0.
+	sp.Value[n] = zeros(sd.NumPts)
 	for n in eachindex(sb.x), z in eachindex(Coef)
 		sp.Value[n] += sb.BF[n][z]*Coef[z]
 	end
